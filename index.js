9433033651fa41f616ae09464ef5fac960132c45
J.R. Mathis <mathiscode@users.noreply.github.com>
2019-04-21 18:38:26 -0500

    catch errors

--- index.js
+++ index.js
@@ -5,14 +5,19 @@ const URLParser = require('url')
 const app = express()
 
 app.use((req, res, next) => {
-  if (!req.query.url) return res.redirect('https://github.com/mathiscode/http-securifier')
+  try {
+    if (!req.query.url) return res.redirect('https://github.com/mathiscode/http-securifier')
 
-  const rawUrl = req.query.url
-  const url = URLParser.parse(rawUrl)
+    const rawUrl = req.query.url
+    const url = URLParser.parse(rawUrl)
 
-  if (!url || !url.hostname) return res.status(400).json({ error: 'Invalid URL' })
+    if (!url || !url.hostname) return res.status(400).json({ error: 'Invalid URL' })
 
-  req.pipe(request(rawUrl)).pipe(res)
+    req.pipe(request(rawUrl, { rejectUnauthorized: false })).pipe(res)
+  } catch (e) {
+    console.error(e)
+    res.status(500).json({ error: 'An error occurred fetching the resource' })
+  }
 })
 
 app.listen(process.env.PORT || 8080, () => console.log('HTTP-Securifier running!'))
